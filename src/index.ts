07ac8d9f2ed83838be44a4f7e38dfb9a5c5b38c9
artemOP <26147889+artemOP@users.noreply.github.com.>
2026-03-12 10:36:57 +0000

    proxy TT API

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { KeyObject } from "node:crypto";
+
+const UPSTREAMS: Record<string, string> = {
+	main: "http://server.tycoon.community:30120",
+	beta: "http://server.tycoon.community:30125",
+};
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async fetch(request: Request): Promise<Response> {
+		const private_key = request.headers.get("x-tycoon-key");
+		if (private_key !== null && (private_key.length < 10 || private_key.length > 50)) {
+			return new Response("Invalid private key", { status: 400 });
+		}
+
+		const public_key = request.headers.get("x-tycoon-public-key");
+		if (public_key !== null && (public_key.length < 10 || public_key.length > 50)) {
+			return new Response("Invalid public key", { status: 400 });
+		}
+
+		const url = new URL(request.url);
+		const [, prefix, ...rest] = url.pathname.split("/");
+
+		const upstream = UPSTREAMS[prefix];
+		if (!upstream) {
+			return new Response("Not found", { status: 404 });
+		}
+
+		const targetUrl = new URL(upstream);
+		url.hostname = targetUrl.hostname;
+		url.port = targetUrl.port;
+		url.protocol = targetUrl.protocol;
+		url.pathname = "/" + rest.join("/");
+
+		return fetch(new Request(url, request));
+	}
+}
